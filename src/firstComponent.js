3f53c321d2c2f90f3bd519153c12f933c449ab3d
Mike <mkejrvs@gmail.com>
2021-07-14 10:40:36 -0500

    first

---
+++ src/firstComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Widget extends React.Component {
+  render() {
+    return (
+      <div>
+	<p>Here's my first react component</p>
+	<p>woo hoo</p>
+      </div>
+    )
+  }
+}
+
+export default Widget;
